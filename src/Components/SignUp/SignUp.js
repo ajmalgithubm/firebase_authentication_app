672d20641df9fac340ee65c3291893f9c1875d75
Ajmal M <136780206+ajmalgithubm@users.noreply.github.com>
2023-07-26 12:51:34 +0530

    create user function is added

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.css'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from '../../Firebase';
 function SignUp() {
-    const []
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const createuser =async (e) => {
+        e.preventDefault();
+        await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+        })
+    }
     return (
         <div>
             <div className="form-container">
@@ -9,13 +19,13 @@ function SignUp() {
                     <h1>Create a account</h1>
                 </div>
                 <div className="email-input">
-                    <input type="text" placeholder='Enter The Email Address' />
+                    <input type="text" placeholder='Enter The Email Address' onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className="password-input">
-                    <input type="password" placeholder='Enter the password' />
+                    <input type="password" placeholder='Enter the password' onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div className="signup-btn">
-                    <button>SignUp</button>
+                    <button onClick={(e) => createuser(e)}>SignUp</button>
                 </div>
             </div>
         </div>
